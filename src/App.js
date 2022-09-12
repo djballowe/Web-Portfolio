cba152de5fff537b7298fd715e592dcb7a5919d6
djballowe <djballowe@gmail.com>
2022-09-12 14:18:52 -0700

    intro layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
 import css from "./App.css";
+import Intro from "./components/intro/intro";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
