d4bddb731756d8936bf600887467fe3876022c15
djballowe <djballowe@gmail.com>
2022-09-14 15:53:35 -0700

    intro animations added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,12 @@
 import React from "react";
+import GitHub from "@mdi/react";
+import { mdiGithub } from "@mdi/js";
+import Linkedin from "@mdi/react";
+import { mdiLinkedin } from "@mdi/js";
+import Resume from "@mdi/react";
+import { mdiFileOutline } from "@mdi/js";
+import Email from "@mdi/react";
+import { mdiEmailOutline } from "@mdi/js";
 
 export default function Contact(props) {
   return (
@@ -13,6 +21,17 @@ export default function Contact(props) {
             Whether you have a question or a project to work on. Send me an
             Email and I'll get back to you as soon as possible.
           </p>
+          <div className="contact-icons">
+            <a href="https://github.com/djballowe">
+              <GitHub path={mdiGithub} size={1.5} />
+            </a>
+            <a href="https://www.linkedin.com/in/david-ballowe-90a164178/">
+              <Linkedin path={mdiLinkedin} size={1.5} />
+            </a>
+            <a href="">
+              <Resume path={mdiFileOutline} size={1.5} />
+            </a>
+          </div>
           <button
             onClick={() => {
               window.location.href = "mailto:db@davidballowe.com";
@@ -21,6 +40,9 @@ export default function Contact(props) {
             Contact
           </button>
         </div>
+      </div>
+      <div className="footer">
+        <p>Designed & Built by David Ballowe</p>
       </div>
     </div>
   );
